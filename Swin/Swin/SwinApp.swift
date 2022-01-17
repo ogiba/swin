f20b3fb0c7c3bed9087e0145e96bb9cf3ff9a8bc
Robert Ogiba <ogiback@gmail.com>
2022-01-17 21:52:36 +0100

    Project initialization

--- Swin/Swin/SwinApp.swift
+++ Swin/Swin/SwinApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct SwinApp: App {
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Swin/Swin/core/Swin.swift
@@ -0,0 +1,15 @@
+//
+//  Swin.swift
+//  Swin
+//
+//  Created by Robert Ogiba on 17/01/2022.
+//
+
+import Foundation
+
+class Swin {
+    
+    static func startSwin() {
+        let swin = Swin()
+    }
+}
