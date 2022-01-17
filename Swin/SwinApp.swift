c5d7bab190c378c1ecc78a72a612f16c421b8035
Robert Ogiba <ogiback@gmail.com>
2022-01-17 21:40:20 +0100

    Initial Commit

---
+++ Swin/SwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwinApp.swift
+//  Swin
+//
+//  Created by Robert Ogiba on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
