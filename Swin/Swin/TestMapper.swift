8970d3bdcbe35bb6028c5384c47a2c875c8e6d5e
Robert Ogiba <ogiback@gmail.com>
2022-01-20 00:40:17 +0100

    Create example mapper required to check injecting classes into module declaratiions

---
+++ Swin/Swin/TestMapper.swift
@@ -0,0 +1,15 @@
+//
+//  TestMapper.swift
+//  Swin
+//
+//  Created by Robert Ogiba on 20/01/2022.
+//
+
+import Foundation
+
+class TestMapper {
+    
+    func map() {
+        print("Mapping some item...")
+    }
+}
