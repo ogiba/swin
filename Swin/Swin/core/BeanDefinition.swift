7db7f2060a0cbbbde95fa53f16c94073a04002bc
Robert Ogiba <ogiback@gmail.com>
2022-01-23 00:35:46 +0100

    Create BeanDefinition struct

---
+++ Swin/Swin/core/BeanDefinition.swift
@@ -0,0 +1,13 @@
+//
+//  BeanDefinition.swift
+//  Swin
+//
+//  Created by Robert Ogiba on 23/01/2022.
+//
+
+import Foundation
+
+struct BeanDefinition {
+    let clazzType: Any
+    let definition: Definition<Any>
+}
