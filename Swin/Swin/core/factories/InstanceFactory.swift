06cf21ced5764f4936a2e1a8148c56eb9a44e3ff
Robert Ogiba <ogiback@gmail.com>
2022-01-26 01:09:44 +0100

    Create struct required to hash instance factory

--- Swin/Swin/core/factories/InstanceFactory.swift
+++ Swin/Swin/core/factories/InstanceFactory.swift
@@ -8,8 +8,21 @@
 import Foundation
 
 protocol InstanceFactory {
+    var identifier: String {get}
     var beanDefinition: BeanDefinition {get set}
     func create<T>(ofType: T.Type) -> T
     func drop()
     func dropAll()
+}
+
+struct HashableInstanceFactory: Hashable {
+    let instanceFactory: InstanceFactory
+    
+    static func == (lhs: HashableInstanceFactory, rhs: HashableInstanceFactory) -> Bool {
+        return lhs.instanceFactory.identifier.isEqual(rhs.instanceFactory.identifier)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(instanceFactory.identifier)
+    }
 }
